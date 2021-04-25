e3fb3f9e3105e1568b478f8a695b96a6108207e7
srinivas <srinivasgangineni18@gmail.com>
2021-04-25 16:06:41 -0400

    Loading the data into Neo4j database

--- loadCompany.py
+++ loadCompany.py
@@ -43,7 +43,6 @@ def main():
         MERGE (a:Project {Name:$proj_name, Number:$proj_no, Location:$proj_location})
         MERGE (dept)-[r:controls]->(a)
         MERGE (a)-[r1:controlled_by]->(dept)
-        
                 ''', parameters = {'proj_dno': int(l[3]), 'proj_name': l[0], 'proj_no': l[1], 'proj_location': l[2]})
 
 
@@ -51,14 +50,105 @@ def main():
         data = f.readlines()
     data = [i.strip().split(':') for i in data]
     for l in data:
-        tx.evaluate('''
-        MATCH (a:Department {Number:$works_for})
-        MATCH (p:Employee)
-        MERGE (a)-[r:employs]->(p)
-        MERGE (p)-[r1:works_for]->(a)
-        SET p += {Fname:$first_name, Minit:$middle_name, Lname:$last_name, Ssn:$ssn, DOB:$dob, Address:$address, Gender:$gender, Salary:$salary}
-                ''', parameters = {'first_name': l[0], 'middle_name': l[1], 'last_name': l[2], 'ssn': l[3], 'dob': l[4], 'address': l[5], 'gender': l[6], 'salary': l[7], 'works_for':l[9]})
+        result =  tx.evaluate('MATCH (a:Employee {Ssn:$emp_ssn}) return a', parameters= {'emp_ssn':l[3]})
+        
+        if result:
+            tx.evaluate('''
+                        MATCH (p:Employee {Ssn:$emp_ssn})
+                        SET p += {Fname:$first_name, Minit:$middle_name, Lname:$last_name, DOB:$dob, Address:$address, Gender:$gender, Salary:$salary}
+                    ''', parameters = {'first_name': l[0] 
+                                        ,'middle_name': l[1]
+                                        ,'last_name': l[2]
+                                        ,'emp_ssn':l[3]
+                                        ,'dob': l[4] 
+                                        ,'address': l[5]
+                                        ,'gender': l[6] 
+                                        ,'salary': l[7]
+                                        ,'dept': int(l[9])
+                        })
 
+        elif l[8] == "null":
+            print(l)
+            tx.evaluate('''
+                        MATCH (d:Department {Number:$dept})
+                        MERGE (e:Employee {Fname:$first_name, Minit:$middle_name, Lname:$last_name, DOB:$dob, Address:$address, Gender:$gender, Salary:$salary, Ssn:$ssn})
+                        MERGE (e)-[r3:works_for]->(d)
+                        MERGE (e)<-[r2:employs]-(d)
+                    ''', parameters = {'first_name': l[0] 
+                                        ,'middle_name': l[1]
+                                        ,'last_name': l[2]
+                                        ,'ssn':l[3]
+                                        ,'dob': l[4] 
+                                        ,'address': l[5]
+                                        ,'gender': l[6] 
+                                        ,'salary': l[7]
+                                        ,'dept': int(l[9])
+                        })
+        else:
+            tx.evaluate('''
+                        MATCH (boss:Employee {Ssn:$man_ssn})
+                        MATCH (d:Department {Number:$dept})
+                        MERGE (e:Employee {First_Name:$first_name, Middle_intial:$middle_name, Last_Name:$last_name, Ssn:$ssn, DOB:$dob, Address:$address, Gender:$gender, Salary:$salary})
+                        MERGE (e)-[r:supervisee]->(boss)
+                        MERGE (boss)-[r1:supervisor]->(e)
+                        MERGE (e)-[r3:works_for]->(d)
+                        MERGE (e)<-[r2:employs]-(d)
+                        
+                    ''', parameters = {'first_name': l[0] 
+                                        ,'middle_name': l[1]
+                                        ,'last_name': l[2]
+                                        ,'ssn': l[3] 
+                                        ,'dob': l[4] 
+                                        ,'address': l[5]
+                                        ,'gender': l[6] 
+                                        ,'salary': l[7] 
+                                        ,'works_for':l[8]
+                                        ,'man_ssn':l[8]
+                                        ,'dept': int(l[9])
+                        })
+    with open('./data/DEPENDENTS.dat') as f:
+        data = f.readlines()
+    data = [i.strip().split(':') for i in data]
+    for l in data:
+        tx.evaluate("""
+            MATCH (e:Employee {Ssn:$emp_ssn})
+            MERGE (d:Dependent {Name:$name, Sex:$sex, BirthDate:$dob, Relationship:$rel})
+            MERGE (e)-[r:dependent]->(d)
+            MERGE (d)-[r1:dependent_of]->(e)
+        """,parameters = {
+            'emp_ssn': l[0]
+            ,'name': l[1]
+            ,'sex': l[2]
+            ,'dob': l[3]
+            ,'rel': l[4]
+        })
+    
+    with open('./data/WORKS_ON.dat') as f:
+        data = f.readlines()
+    data = [i.strip().split(':') for i in data]
+    for l in data:
+        tx.evaluate("""
+            MATCH (e:Employee {Ssn:$emp_ssn})
+            MATCH (p:Project {Number: $proj_number})
+            MERGE (e)-[r:works_on {Hours: $hours}]->(p)
+            MERGE (e)<-[r1:worker {Hours: $hours}]-(p)
+        """,parameters = {
+            'emp_ssn': l[0]
+            ,'proj_number': l[1]
+            ,'hours': l[2]
+        })
+
+    with open('./data/DEPT_LOCATIONS.dat') as f:
+        data = f.readlines()
+    data = [i.strip().split(':') for i in data]
+    for l in data:
+        tx.evaluate("""
+            MATCH (d:Department {Number:$number})
+            SET d += {Location: $locations}
+        """,parameters = {
+            'number': int(l[0])
+            ,'locations': l[1:]
+        })
     
     tx.commit()
 
@@ -73,3 +163,10 @@ if __name__ == "__main__":
 
 #MATCH (dept:Department {dept_no:$proj_dno}) MERGE (dept)-[r:controls]->(a)
        # MERGE (a)-[r1:controlled_by]->(dept)
+
+
+# MATCH (a:Department {Number:$works_for})
+# MATCH (p:Employee)
+# MERGE (a)-[r:employs]->(p)
+# MERGE (p)-[r1:works_for]->(a)
+# SET p += {Fname:$first_name, Minit:$middle_name, Lname:$last_name, Ssn:$ssn, DOB:$dob, Address:$address, Gender:$gender, Salary:$salary}
